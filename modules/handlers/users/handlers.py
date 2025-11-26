ef8fff94a63551a911113cc2899e1d19630aeb23
Rovshen Berdimyradov <rovshen.berdimyradov@emergn.com>
2025-11-26 11:30:16 +0500

    change sequence of qr code and mini info about user

--- modules/handlers/users/handlers.py
+++ modules/handlers/users/handlers.py
@@ -2974,6 +2974,19 @@ async def finish_create_user(update: Update, context: ContextTypes.DEFAULT_TYPE)
         for key in ("create_user", "create_user_fields", "current_field_index", "using_template", "search_type", "waiting_for"):
             context.user_data.pop(key, None)
 
+        # Send QR/crypto link first, before the success message
+        if crypto_link:
+            qr_stream = _build_qr_code_payload(crypto_link)
+            username_md = escape_markdown(result.get('username', ''))
+            caption = f"🔳 QR-код для `{username_md}`\n`{escape_markdown(crypto_link)}`"
+            target_message = update.callback_query.message if update.callback_query else update.message
+            if target_message:
+                await target_message.reply_photo(photo=qr_stream, caption=caption, parse_mode="Markdown")
+            elif update.effective_chat:
+                await update.effective_chat.send_photo(photo=qr_stream, caption=caption, parse_mode="Markdown")
+            else:
+                logger.warning("Unable to send QR code photo after user creation")
+
         if update.callback_query:
             await update.callback_query.edit_message_text(
                 text=message,
@@ -2986,17 +2999,6 @@ async def finish_create_user(update: Update, context: ContextTypes.DEFAULT_TYPE)
                 reply_markup=reply_markup,
                 parse_mode="Markdown"
             )
-        if crypto_link:
-            qr_stream = _build_qr_code_payload(crypto_link)
-            username_md = escape_markdown(result.get('username', ''))
-            caption = f"🔳 QR-код для `{username_md}`\n`{escape_markdown(crypto_link)}`"
-            target_message = update.callback_query.message if update.callback_query else update.message
-            if target_message:
-                await target_message.reply_photo(photo=qr_stream, caption=caption, parse_mode="Markdown")
-            elif update.effective_chat:
-                await update.effective_chat.send_photo(photo=qr_stream, caption=caption, parse_mode="Markdown")
-            else:
-                logger.warning("Unable to send QR code photo after user creation")
 
         if created_uuid:
             try:
